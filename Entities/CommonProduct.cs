0e0107124f3073f7ef133290c36858fce3edf142
irineusjr <31733869+irineusjr@users.noreply.github.com>
2020-05-02 16:33:16 -0300

    Add files via upload

---
+++ Entities/CommonProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Product.Entities
+{
+    class CommonProduct
+    {
+        public string Name { get; set; }
+        public double Price{ get; set; }
+
+        public CommonProduct() 
+        {
+        }
+
+        public CommonProduct(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag() 
+        {
+            return Name + " $ " + Price.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Product.Entities;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of products: ");
+            int numProds = int.Parse(Console.ReadLine());
+            List<CommonProduct> ProdList = new List<CommonProduct>();
+            
+
+            for (int i = 1; i <= numProds; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char prodType = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (prodType == 'c' || prodType == 'C')
+                {
+                    ProdList.Add(new CommonProduct(name, price));
+                }
+                else if (prodType == 'u' || prodType == 'U')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    ProdList.Add(new UsedProduct(name, price, date));
+                }
+                else if (prodType == 'i' || prodType == 'I')
+                {
+                    Console.Write("Customs fee: ");
+                    double fee = double.Parse(Console.ReadLine());
+                    ProdList.Add(new ImportedProduct(name, price, fee));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (CommonProduct prod in ProdList)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+
+        }
+    }
+}
